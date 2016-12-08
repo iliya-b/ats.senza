270d80c015f8f670d3232e6d88562be426e130b3
Marco Mariani <marco.mariani@alterway.fr>
2016-12-08 14:54:03 +0100

    version bump, copyright

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -65,7 +65,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'ats-senza'
-copyright = '2015, author-name'
+copyright = '2016, AiC Project'
 author = 'AiC Project'
 
 # The version info for the project you're documenting, acts as replacement for
